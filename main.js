71eeb71621d2edbd112a1017c0e6c9ce24ea525a
prince-deb <78799922+prince-deb@users.noreply.github.com>
2021-10-07 18:52:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
+
+song="";
 function setup(){
-    canvas=createCanvas(550,550);
-     canvas.position(560,150);
+    canvas=createCanvas(600,500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+}
+function draw(){
+    image(video,0,0,600,500);
+}
+function preload(){
+song=loadSound("spiderman.mp3.mp3");
+}
+function play(){
+song.play();
 }
